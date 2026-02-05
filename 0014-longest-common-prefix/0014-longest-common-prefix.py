9c3160328bf3fd11b5cfd248fc062e84fc19138f
Ashish Pise <153230903+offbeatash@users.noreply.github.com>
2026-02-05 23:40:58 +0530

    Time: 0 ms (100%), Space: 19.4 MB (39.7%) - LeetHub

--- 0014-longest-common-prefix/0014-longest-common-prefix.py
+++ 0014-longest-common-prefix/0014-longest-common-prefix.py
@@ -3,11 +3,12 @@ class Solution:
         if not strs:
             return ""
         
-        s1 = min(strs)
-        s2 = max(strs)
+        first = strs[0]
         
-        for i, c in enumerate(s1):
-            if c != s2[i]:
-                return s1[:i]
+        for i in range(len(first)):
+            ch = first[i]
+            for j in range(1, len(strs)):
+                if i >= len(strs[j]) or strs[j][i] != ch:
+                    return first[:i]
         
-        return s1
+        return first
