aefae5b8ee781eca2b5afd5609d278c5d6409921
Ashish Pise <153230903+offbeatash@users.noreply.github.com>
2026-08-04 00:51:45 +0530

    Time: 502 ms (58.76%), Space: 0B (100%) - LeetHub

--- 0584-find-customer-referee/0584-find-customer-referee.sql
+++ 0584-find-customer-referee/0584-find-customer-referee.sql
@@ -1,2 +1,4 @@
-/* Write your T-SQL query statement below */
-SELECT name FROM Customer where referee_id != 2 OR referee_id is NULL ;
+SELECT name
+FROM Customer
+WHERE referee_id <> 2
+   OR referee_id IS NULL;
