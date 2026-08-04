8a656ec4f1e9317632af9dc386d5722aac0c1dc0
Ashish Pise <153230903+offbeatash@users.noreply.github.com>
2026-08-05 00:27:47 +0530

    Time: 3 ms (84.92%), Space: 21.2 MB (47.85%) - LeetHub

--- 0169-majority-element/0169-majority-element.py
+++ 0169-majority-element/0169-majority-element.py
@@ -1,3 +1,9 @@
 class Solution:
     def majorityElement(self, nums: List[int]) -> int:
-        return Counter(nums).most_common(1)[0][0]
+        count = 0
+
+        for num in nums:
+            if count == 0:
+                candidate = num
+            count += 1 if num==candidate else -1
+        return candidate
