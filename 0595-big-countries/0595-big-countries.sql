cb5b381aa3c8037718a4d157143da04870704b50
Ashish Pise <153230903+offbeatash@users.noreply.github.com>
2026-08-05 00:43:44 +0530

    Time: 646 ms (31.74%), Space: 0B (100%) - LeetHub

--- 0595-big-countries/0595-big-countries.sql
+++ 0595-big-countries/0595-big-countries.sql
@@ -1,5 +1,5 @@
 /* Write your T-SQL query statement below */
 SELECT name, area, population
 FROM World
-WHERE area >= 3000000
-OR population >= 25000000;
+WHERE area > 2999999
+OR population >= 24999999;
