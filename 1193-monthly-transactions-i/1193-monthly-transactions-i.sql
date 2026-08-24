b83a11498a1f2ce39ab8fefaa50e2917f16dcb6b
Ashish Pise <153230903+offbeatash@users.noreply.github.com>
2026-08-24 15:31:49 +0530

    Time: 1071 ms (86.13%), Space: 0B (100%) - LeetHub

--- 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
+++ 1193-monthly-transactions-i/1193-monthly-transactions-i.sql
@@ -1,12 +1,12 @@
 /* Write your T-SQL query statement below */
 SELECT
-    FORMAT(trans_date, 'yyyy-MM') AS month,
+    CONVERT(char(7), trans_date, 120) AS month,
     country,
     COUNT(*) AS trans_count,
-    SUM(CASE WHEN state ='approved' THEN 1 ELSE 0 END) AS approved_count,
+    SUM(CASE WHEN state = 'approved' THEN 1 ELSE 0 END) AS approved_count,
     SUM(amount) AS trans_total_amount,
-    SUM(CASE WHEN state='approved' THEN amount ELSE 0 END) AS approved_total_amount
+    SUM(CASE WHEN state = 'approved' THEN amount ELSE 0 END) AS approved_total_amount
 FROM Transactions
-GROUP BY 
-    FORMAT(trans_date, 'yyyy-MM'),
+GROUP BY
+    CONVERT(char(7), trans_date, 120),
     country;
