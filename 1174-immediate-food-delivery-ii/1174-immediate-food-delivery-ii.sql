9241f4b7391403954a1d932bff7d56ca4a98c282
Ashish Pise <153230903+offbeatash@users.noreply.github.com>
2026-08-25 18:20:08 +0530

    Time: 1053 ms (38.73%), Space: 0B (100%) - LeetHub

--- 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
+++ 1174-immediate-food-delivery-ii/1174-immediate-food-delivery-ii.sql
@@ -1,16 +1,14 @@
-/* Write your T-SQL query statement below */
-SELECT
-    ROUND(
-        100.00 * SUM(
-            CASE WHEN customer_pref_delivery_date = order_date THEN 1 ELSE 0 END)
-        /COUNT(*),2)
-        AS immediate_percentage
-FROM (
-    SELECT*,
-    ROW_NUMBER() OVER(
-        PARTITION BY customer_id
-        ORDER BY order_date
-    ) AS rn
+SELECT ROUND(
+    100.0 * SUM(
+        CASE 
+            WHEN order_date = customer_pref_delivery_date THEN 1 
+            ELSE 0 
+        END
+    ) / COUNT(*), 2
+) AS immediate_percentage
+FROM Delivery d
+WHERE order_date = (
+    SELECT MIN(order_date)
     FROM Delivery
-) d
-WHERE rn=1;
+    WHERE customer_id = d.customer_id
+);
