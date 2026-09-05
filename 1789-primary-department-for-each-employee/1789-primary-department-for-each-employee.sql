839eec45d7e7f6fbb747f73139e7cb37aae63250
Ashish Pise <153230903+offbeatash@users.noreply.github.com>
2026-09-05 14:15:19 +0530

    Time: 419 ms (90.67%), Space: 0B (100%) - LeetHub

---
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -0,0 +1,10 @@
+/* Write your T-SQL query statement below */
+SELECT employee_id, department_id
+FROM Employee
+WHERE primary_flag = 'Y'
+    OR employee_id in(
+        SELECT employee_id
+        FROM Employee
+        GROUP BY employee_id
+        HAVING COUNT(*) = 1
+    );
