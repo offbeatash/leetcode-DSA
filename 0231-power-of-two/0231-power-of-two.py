8f395696dbb6a713174a7051f6f36783f48bc769
Ashish Pise <153230903+offbeatash@users.noreply.github.com>
2026-09-09 13:27:59 +0530

    Time: 0 ms (100%), Space: 19.1 MB (97.83%) - LeetHub

--- 0231-power-of-two/0231-power-of-two.py
+++ 0231-power-of-two/0231-power-of-two.py
@@ -1,3 +1,7 @@
 class Solution:
     def isPowerOfTwo(self, n: int) -> bool:
         return n > 0 and  (n &(n-1)) == 0
+
+#using bitwise and(&) operator.
+#n must be +ve, and power of 2 has only '1' in binary.
+#n &(n-1) removes that '1', so result becomes 0, means power of 2.
