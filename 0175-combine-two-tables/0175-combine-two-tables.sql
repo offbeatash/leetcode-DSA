3f48acfcd874262693ff217021a1bee9f4d84569
Ashish Pise <153230903+offbeatash@users.noreply.github.com>
2026-09-24 11:52:42 +0530

    Time: 420 ms (77.4%), Space: 0B (100%) - LeetHub

---
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -0,0 +1,9 @@
+/* Write your T-SQL query statement below */
+SELECT 
+    p.firstName,
+    p.lastName,
+    a.city,
+    a.state
+FROM Person AS p
+LEFT JOIN Address AS a
+    ON p.personId = a.personId;
