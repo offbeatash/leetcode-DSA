0548be2d3bc17dea640d4d57ec5e088182a249e7
Ashish Pise <153230903+offbeatash@users.noreply.github.com>
2026-09-24 11:54:14 +0530

    Time: 258 ms (78.33%), Space: 0B (100%) - LeetHub

--- 0620-not-boring-movies/0620-not-boring-movies.sql
+++ 0620-not-boring-movies/0620-not-boring-movies.sql
@@ -1,4 +1,10 @@
 -- Write your T-SQL query statement below
-Select * from Cinema
-where description != 'boring' and id % 2 != 0
-order by rating desc;
+SELECT
+    id,
+    movie,
+    description,
+    rating
+FROM Cinema
+WHERE id % 2 = 1
+  AND description <> 'boring'
+ORDER BY rating DESC;
